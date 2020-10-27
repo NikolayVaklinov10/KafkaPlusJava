41a22bac7166043bfdf515f5b5556fc31e9c67bf
Nick <nikolayvaklinov@yahoo.com>
2020-10-27 23:07:38 +0000

    the properties needed for the Producer

--- src/main/java/com/kafkaPlusJava/playground/ProducerDemo.java
+++ src/main/java/com/kafkaPlusJava/playground/ProducerDemo.java
@@ -1,12 +1,22 @@
 package com.kafkaPlusJava.playground;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
-        System.out.println("So far so good, lets get started");
+
+        String bootstrapServers = "127.0.0.1:9092";
 
         // THE FOLLOWING 3 STEPS TO MAKE A PRODUCER
         // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // create the producer
 

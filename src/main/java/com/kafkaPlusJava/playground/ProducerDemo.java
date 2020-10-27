154aabc9fe881786d52306c86e486a3d88a80803
Nick <nikolayvaklinov@yahoo.com>
2020-10-27 20:55:43 +0000

    testing whether everything is ok

---
+++ src/main/java/com/kafkaPlusJava/playground/ProducerDemo.java
@@ -0,0 +1,8 @@
+package com.kafkaPlusJava.playground;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        System.out.println("So far so good, lets get started");
+    }
+}

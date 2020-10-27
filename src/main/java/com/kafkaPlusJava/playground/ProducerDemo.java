67d2033180a0f812a77de0a75b3cfa0f4cbd0ead
Nick <nikolayvaklinov@yahoo.com>
2020-10-27 22:49:55 +0000

    the way a producer is made

--- src/main/java/com/kafkaPlusJava/playground/ProducerDemo.java
+++ src/main/java/com/kafkaPlusJava/playground/ProducerDemo.java
@@ -4,5 +4,12 @@ public class ProducerDemo {
 
     public static void main(String[] args) {
         System.out.println("So far so good, lets get started");
+
+        // THE FOLLOWING 3 STEPS TO MAKE A PRODUCER
+        // create Producer properties
+
+        // create the producer
+
+        // send data
     }
 }

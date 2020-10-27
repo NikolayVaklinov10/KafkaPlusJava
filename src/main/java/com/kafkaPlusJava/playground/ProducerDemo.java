77a9b465503ff3fee6774b784590333a8951081c
Nick <nikolayvaklinov@yahoo.com>
2020-10-27 23:37:24 +0000

    the simple producer code

--- src/main/java/com/kafkaPlusJava/playground/ProducerDemo.java
+++ src/main/java/com/kafkaPlusJava/playground/ProducerDemo.java
@@ -2,6 +2,7 @@ package com.kafkaPlusJava.playground;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -22,6 +23,15 @@ public class ProducerDemo {
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
+        // create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world");
+
         // send data
+        producer.send(record);
+
+        // flush data
+        producer.flush();
+        // flush and close
+        producer.close();
     }
 }

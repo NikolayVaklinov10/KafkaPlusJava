248a3c9423506a41acb71df23b9aec2abf6187f9
Nick <nikolayvaklinov@yahoo.com>
2020-10-28 13:27:40 +0000

    consumer demo beginning

---
+++ src/main/java/com/kafkaPlusJava/playground/ConsumerDemo.java
@@ -0,0 +1,8 @@
+package com.kafkaPlusJava.playground;
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        // just for checking whether everything is working
+        System.out.println("Hello, world");
+    }
+}

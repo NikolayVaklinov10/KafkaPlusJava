99c38e2689ad7d233d7cdc243297842331ac1c1b
Nick <nikolayvaklinov@yahoo.com>
2020-10-31 22:01:19 +0000

    the unit tests for the twitter app

---
+++ src/test/java/com/kafkaPlusJava/Twitter/TwitterProducerTest.java
@@ -0,0 +1,22 @@
+package com.kafkaPlusJava.Twitter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwitterProducerTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void run() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createTwitterClient() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createKafkaProducer() {
+    }
+}

1b20085481d3c9b01e2f66a2a508beccdc8348ca
Nick <nikolayvaklinov@yahoo.com>
2020-11-01 21:20:09 +0000

    a template for writing some tests for this class

---
+++ src/test/java/com/kafkaPlusJava/playground/ConsumerDemoTest.java
@@ -0,0 +1,12 @@
+package com.kafkaPlusJava.playground;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsumerDemoTest {
+
+    @Test
+    void main() {
+    }
+}
